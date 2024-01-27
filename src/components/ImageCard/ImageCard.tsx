9312610c07507eb1c0711fadd830a6212d708d4e
gilbertoaceville <gilbertoaceville@gmail.com>
2024-01-27 17:43:12 +0100

    feat: complete palette setup

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -1,13 +1,14 @@
 import Image, { StaticImageData } from "next/image";
 import styles from "./ImageCard.module.scss";
 import { convertRgbToHex } from "@/utils/convertRgbtoHex";
+import ColorCard from "./component/ColorCard";
 
-interface HeroImageProps {
+interface ImageCardProps {
   image: StaticImageData | string;
   palette: any[];
 }
 
-export default function HeroImage({ image, palette }: HeroImageProps) {
+export default function ImageCard({ image, palette }: ImageCardProps) {
   return (
     <div className={styles.wrapper}>
       <div className={styles.wrapper__image}>
@@ -23,9 +24,10 @@ export default function HeroImage({ image, palette }: HeroImageProps) {
       {palette.length > 0 && (
         <ul className={styles.wrapper__palette}>
           {palette.map((color, index) => {
-            const rgbColor = `rgb(${color.join(",")})`;
+            const rgb = `rgb(${color.join(",")})`;
             const hex = `#${color.map(convertRgbToHex).join("")}`;
-            return <li></li>;
+
+            return <ColorCard key={`${rgb}-${index}`} {...{ rgb, hex }} />;
           })}
         </ul>
       )}
--- src/components/ImageCard/component/ColorCard.tsx
+++ src/components/ImageCard/component/ColorCard.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
-import styles from "../HeroImage.module.scss";
+import React, { SyntheticEvent, useState } from "react";
 
-export default function ColorCard() {
+interface ColorCardProps {
+  rgb: string;
+  hex: string;  
+}
+
+export default function ColorCard({ rgb, hex }: ColorCardProps) {
+  const [copied, setCopied] = useState(false);
+
+  function copyToClipboard(e: SyntheticEvent) {
+    const color = (e.target as HTMLDivElement).innerText;
+    navigator.clipboard.writeText(color);
+  }
+
+  function handleCopy(e: SyntheticEvent) {
+    copyToClipboard(e);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  }
   return (
-    <div>ColorCard</div>
-  )
+    <li style={{ background: rgb }}>
+      <span
+        onClick={handleCopy}
+      >
+        {copied ? "Copied!" : hex} <i className="far fa-copy fa-beat-fade"></i>
+      </span>
+    </li>
+  );
 }
